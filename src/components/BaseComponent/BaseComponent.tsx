f6d0dcecee8f8b742513fa60f1960d985f52f2a1
r10-artem <r10-artem@monowork.pro>
2024-04-05 00:29:43 +0300

    master: delete base Component

--- src/components/BaseComponent/BaseComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { BaseComponentProps } from "./BaseComponent.interfaces";
-
-const BaseComponent: FC<BaseComponentProps> = (props) => {
-  return <div></div>;
-};
-
-export default BaseComponent;
