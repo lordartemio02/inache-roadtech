f22eb1290b02f4b001915c367f453186291f7da4
r10-artem <r10-artem@monowork.pro>
2024-04-05 00:28:48 +0300

    master: update icon and config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env": process.env,
-  },
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
