948e2611bde66f23f3e2f6ebfedfb8f55c10ebe0
r10-artem <r10-artem@monowork.pro>
2024-04-07 12:48:32 +0300

    master: update img

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import img from "../../assets/images/e.png";
 
 const enum Contents {
   first = "Привет! Отправимся вместе исследовать удивительный город Казань!",
@@ -9,25 +10,27 @@ const Character = () => {
   const [activeContent, setActiveContent] = useState(Contents.first);
 
   const onClick = () => {
-    setActiveContent(activeContent === Contents.first ? Contents.second : Contents.first )
+    setActiveContent(
+      activeContent === Contents.first ? Contents.second : Contents.first
+    );
   };
 
   return (
     <div className="absolute flex items-center flex-row z-[3] top-[110px] h-[112px]">
       <img
         className="inline-block size-[98px] z-[4]"
-        src='src/assets/images/e.png'
+        src={img}
         alt="Character"
         onClick={onClick}
       />
 
-      <div className="
+      <div
+        className="
         w-[226px] bg-natural-800 flex flex-col rounded-16 p-[8px] text-natural-100 text-sm shadow-[4px_0px_16px_0px_rgba(0, 0, 0, 0.25)]
       ">
         {activeContent}
       </div>
     </div>
-
   );
 };
 
